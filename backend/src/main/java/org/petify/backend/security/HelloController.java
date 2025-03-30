df79e1ba5d813b4953b3418d87ca75f04fd18b88
janrzepkowski <jan.rzepkowski21@gmail.com>
2025-03-30 11:30:16 +0200

    Init backend project

---
+++ backend/src/main/java/org/petify/backend/security/HelloController.java
@@ -0,0 +1,12 @@
+package org.petify.backend.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   @GetMapping("/")
+    public String hello() {
+        return "Welcome to Petify!";
+    }
+}
