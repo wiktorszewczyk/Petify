071de857302edd5c132446a58518748e178b0518
Mateusz Kolata <114952178+mkolata13@users.noreply.github.com>
2025-04-06 12:56:08 +0200

    Microservice skeleton (#3)
    * init config-server and discovery
    
    * configured gateway and basic of auth server
    
    * working gateway, added example service - shelter, with some simple data, switched to java 21 as some lombok(?) problems with java 24
    
    * Delete .idea directory
    
    * refactored security module to work as authentiacion server, added config, eureka client dependencies, switched jdk24 -> jdk21
    
    * fixed auth server to work on gateway aswell (:8222)

---
+++ backend/auth-server/src/main/java/org/petify/backend/security/models/ApplicationUser.java
@@ -0,0 +1,110 @@
+package org.petify.backend.security.models;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class ApplicationUser implements UserDetails{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer userId;
+
+	@Column(unique=true)
+	private String username;
+
+	private String password;
+
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(
+			name="user_role_junction",
+			joinColumns = {@JoinColumn(name="user_id")},
+			inverseJoinColumns = {@JoinColumn(name="role_id")}
+	)
+	private Set<Role> authorities;
+
+	@Version
+	private Integer version = 0;
+
+	public ApplicationUser() {
+		super();
+		authorities = new HashSet<>();
+	}
+
+	public ApplicationUser(Integer userId, String username, String password, Set<Role> authorities) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.authorities = authorities;
+	}
+
+	public Integer getUserId() {
+		return this.userId;
+	}
+
+	public void setId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public void setAuthorities(Set<Role> authorities) {
+		this.authorities = authorities;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+}
