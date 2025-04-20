472b57c57f6bb1349d5169c621cc72310233ae34
247813 Tomalak Piotr <247813@edu.p.lodz.pl>
2025-04-20 19:55:33 +0200

    initial commit for funding-service

---
+++ app/backend/funding/src/main/java/org/petify/funding/FundingApplication.java
@@ -0,0 +1,13 @@
+package org.petify.funding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FundingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FundingApplication.class, args);
+    }
+}
