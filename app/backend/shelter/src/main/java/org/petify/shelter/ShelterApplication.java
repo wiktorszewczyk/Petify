fe31b0fae635eae437f0103da12cc104598d3c95
Mateusz Kolata <mkolata13@gmail.com>
2025-04-23 15:24:12 +0200

    auth fix for shelter

--- app/backend/shelter/src/main/java/org/petify/shelter/ShelterApplication.java
+++ app/backend/shelter/src/main/java/org/petify/shelter/ShelterApplication.java
@@ -3,10 +3,12 @@ package org.petify.shelter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.oauth2.resourceserver.OAuth2ResourceServerSecurityMarker;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableMethodSecurity
 public class ShelterApplication {
 
     public static void main(String[] args) {
