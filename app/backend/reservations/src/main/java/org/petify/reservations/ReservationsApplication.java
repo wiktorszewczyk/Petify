6324d8c4451d1922c8d49a80cdb0115f6517e34d
jhynasinski <247674@edu.p.lodz.pl>
2025-04-24 17:30:00 +0200

    add reservations microservice skeleton

---
+++ app/backend/reservations/src/main/java/org/petify/reservations/ReservationsApplication.java
@@ -0,0 +1,13 @@
+package org.petify.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReservationsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationsApplication.class, args);
+    }
+}
