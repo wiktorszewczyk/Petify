96e9ff87ee7ae9d7762b01a5bd3bb81b536be4b8
jhynasinski <247674@edu.p.lodz.pl>
2025-04-28 20:21:22 +0200

    Added an endpoint getAllPetIds, corsConfiguration, changes in feign and fully working batch for slots

--- app/backend/shelter/src/main/java/org/petify/shelter/config/JwtSecurityConfig.java
+++ app/backend/shelter/src/main/java/org/petify/shelter/config/JwtSecurityConfig.java
@@ -35,7 +35,11 @@ public class JwtSecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .oauth2ResourceServer(AbstractHttpConfigurer::disable);
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                );
         return http.build();
     }
 
