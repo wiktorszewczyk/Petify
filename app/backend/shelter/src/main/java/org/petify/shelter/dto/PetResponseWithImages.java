414a82b08f35c2ddb9dae70d2c50edac6a513718
Mateusz Kolata <mkolata13@gmail.com>
2025-05-13 17:57:29 +0200

    fixed image service

---
+++ app/backend/shelter/src/main/java/org/petify/shelter/dto/PetResponseWithImages.java
@@ -0,0 +1,29 @@
+package org.petify.shelter.dto;
+
+import org.petify.shelter.enums.Gender;
+import org.petify.shelter.enums.PetType;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * DTO for {@link org.petify.shelter.model.Pet}
+ */
+public record PetResponseWithImages(
+        Long id,
+        String name,
+        PetType type,
+        String breed,
+        Integer age,
+        boolean archived,
+        String description,
+        Long shelterId,
+        Gender gender,
+        boolean vaccinated,
+        boolean urgent,
+        boolean sterilized,
+        boolean kidFriendly,
+        String imageName,
+        String imageType,
+        byte[] imageData,
+        List<PetImageResponse> images) implements Serializable {}
--- app/backend/shelter/src/main/java/org/petify/shelter/mapper/PetImageMapper.java
+++ app/backend/shelter/src/main/java/org/petify/shelter/mapper/PetImageMapper.java
@@ -1,22 +1,32 @@
 package org.petify.shelter.mapper;
 
+import org.mapstruct.*;
 import org.petify.shelter.dto.PetImageRequest;
 import org.petify.shelter.dto.PetImageResponse;
+import org.petify.shelter.model.Pet;
 import org.petify.shelter.model.PetImage;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import java.util.Base64;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PetImageMapper {
-    PetImage toEntity(PetImageRequest petImageRequest);
+
+    @Mapping(target = "id", ignore = true)
+    PetImage toEntity(PetImageRequest petImageRequest, @Context Pet pet);
 
     PetImageResponse toDto(PetImage petImage);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    PetImage partialUpdate(PetImageRequest petImageRequest, @MappingTarget PetImage petImage);
+    default PetImage toEntityWithPet(PetImageRequest request, Pet pet) {
+        PetImage petImage = toEntity(request, pet);
+        petImage.setPet(pet);
+        return petImage;
+    }
+
+    default byte[] map(String imageData) {
+        return Base64.getDecoder().decode(imageData);
+    }
+
+    default String map(byte[] imageData) {
+        return Base64.getEncoder().encodeToString(imageData);
+    }
 }
