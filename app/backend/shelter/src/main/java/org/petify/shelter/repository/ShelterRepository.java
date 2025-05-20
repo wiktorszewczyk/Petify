5b372f1458782e2cff89b4aa25d72d1ec3025601
Mateusz Kolata <mkolata13@gmail.com>
2025-05-20 22:00:16 +0200

    fixes for checkstyle, encoding

--- app/backend/shelter/src/main/java/org/petify/shelter/repository/ShelterRepository.java
+++ app/backend/shelter/src/main/java/org/petify/shelter/repository/ShelterRepository.java
@@ -1,6 +1,7 @@
 package org.petify.shelter.repository;
 
 import org.petify.shelter.model.Shelter;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface ShelterRepository extends JpaRepository<Shelter, Long> {
+
     Optional<Shelter> getShelterByOwnerUsername(String ownerUsername);
 }
