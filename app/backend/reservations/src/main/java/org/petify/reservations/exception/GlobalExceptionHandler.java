cd9f6bd284750c9eceb4929c145a8726d2b47ce0
jhynasinski <247674@edu.p.lodz.pl>
2025-05-28 19:12:03 +0200

    fix

--- app/backend/reservations/src/main/java/org/petify/reservations/exception/GlobalExceptionHandler.java
+++ app/backend/reservations/src/main/java/org/petify/reservations/exception/GlobalExceptionHandler.java
@@ -5,14 +5,13 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(SlotNotFoundException.class)
     public ProblemDetail handleSlotNotFound(SlotNotFoundException ex) {
