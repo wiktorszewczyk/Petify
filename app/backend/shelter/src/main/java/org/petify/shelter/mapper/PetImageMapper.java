236b8d2bb9112ca30e11b36afd292d0ca76bafdd
Mateusz Kolata <mkolata13@gmail.com>
2025-06-01 11:41:57 +0200

    added sample data and fixed returning images with null data

--- app/backend/shelter/src/main/java/org/petify/shelter/mapper/PetImageMapper.java
+++ app/backend/shelter/src/main/java/org/petify/shelter/mapper/PetImageMapper.java
@@ -1,24 +1,28 @@
 package org.petify.shelter.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 import org.petify.shelter.dto.PetImageResponse;
 import org.petify.shelter.model.PetImage;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
 
 import java.util.Base64;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PetImageMapper {
 
+    @Mapping(source = "imageData", target = "imageData", qualifiedByName = "encodeBase64")
     PetImageResponse toDto(PetImage petImage);
 
-    default byte[] map(String imageData) {
-        return Base64.getDecoder().decode(imageData);
+    @Named("encodeBase64")
+    default String encodeBase64(byte[] imageData) {
+        return Base64.getEncoder().encodeToString(imageData);
     }
 
-    default String map(byte[] imageData) {
-        return Base64.getEncoder().encodeToString(imageData);
+    @Named("decodeBase64")
+    default byte[] decodeBase64(String imageData) {
+        return Base64.getDecoder().decode(imageData);
     }
 }
