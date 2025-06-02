a908f1badb08071453399d129132ca56133d5cdc
Mateusz Kolata <114952178+mkolata13@users.noreply.github.com>
2025-06-02 20:43:58 +0200

    added sample data and fixed returning images with null data (#23)

--- app/backend/shelter/src/main/java/org/petify/shelter/mapper/PetMapper.java
+++ app/backend/shelter/src/main/java/org/petify/shelter/mapper/PetMapper.java
@@ -6,7 +6,6 @@ import org.petify.shelter.dto.PetResponseWithImages;
 import org.petify.shelter.model.Pet;
 import org.petify.shelter.model.Shelter;
 
-import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -15,10 +14,14 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = PetImageMapper.class)
 public interface PetMapper {
     @Mapping(source = "shelter.id", target = "shelterId")
+    @Mapping(target = "imageData", expression = "java(pet.getImageData() != null ?"
+            + " java.util.Base64.getEncoder().encodeToString(pet.getImageData()) : null)")
     PetResponse toDto(Pet pet);
 
     @Mapping(source = "shelter.id", target = "shelterId")
     @Mapping(target = "images", source = "images")
+    @Mapping(target = "imageData", expression = "java(pet.getImageData() != null ?"
+            + " java.util.Base64.getEncoder().encodeToString(pet.getImageData()) : null)")
     PetResponseWithImages toDtoWithImages(Pet pet);
 
     @Mapping(target = "id", ignore = true)
@@ -26,11 +29,10 @@ public interface PetMapper {
     @Mapping(target = "images", ignore = true)
     @Mapping(target = "favoritePets", ignore = true)
     @Mapping(target = "archived", ignore = true)
-    @Mapping(target = "shelter", ignore = true)
-    Pet toEntity(PetRequest request, @Context Shelter shelter);
+    Pet toEntity(PetRequest request);
 
     default Pet toEntityWithShelter(PetRequest request, Shelter shelter) {
-        Pet pet = toEntity(request, shelter);
+        Pet pet = toEntity(request);
         pet.setShelter(shelter);
         return pet;
     }
