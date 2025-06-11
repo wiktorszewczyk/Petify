a2d16ac798d1367834b207e07165f57b25d36d1b
Jan Rzepkowski <jan.rzepkowski21@gmail.com>
2025-06-11 11:46:08 +0200

    Geolocation search, CRUD operations for profile picture, logic for likes achievements & some fixes (#27)
    * Add Google OAuth2 login functionality
    
    * Data flow (JWT) through gateway, DRY code & sample user-service module
    
    * Add Google OAuth2 login functionality
    
    # Conflicts:
    #	.gitignore
    #	app/backend/auth-server/src/main/java/org/petify/backend/security/configuration/SecurityConfiguration.java
    #	app/backend/auth-server/src/main/java/org/petify/backend/security/controllers/AuthenticationController.java
    #	app/backend/auth-server/src/main/resources/application.yml
    #	app/backend/config-server/src/main/resources/configurations/auth-service.yml
    
    * Data flow (JWT) through gateway, DRY code & sample user-service module
    
    * Simplified configuration
    
    * Basic data for registration, login by phone number or email
    
    * User CRUD & new naming endpoints
    
    * Add volunteer management and enhanced user roles
    
    * Add user account activation/deactivation functionality
    
    * Some changes
    
    * Implement user achievements, levels & badges tracking
    
    * Fix google login
    
    * Fixing checkstyle errors
    
    * Sisyphus works
    
    * User geolocation functionality
    
    * Minor changes
    
    * Google login adjustments
    
    * Feign client for achievement tracking between shelter and auth services & code cleanup
    
    * handle CRUD operations for profile image with PostgreSQL compatibility
    
    * Google oauth2 fix for mobile

---
+++ app/backend/shelter/src/main/java/org/petify/shelter/config/FeignConfig.java
@@ -0,0 +1,23 @@
+package org.petify.shelter.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor() {
+        return template -> {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth instanceof JwtAuthenticationToken jwtAuth) {
+                String token = jwtAuth.getToken().getTokenValue();
+                template.header("Authorization", "Bearer " + token);
+            }
+        };
+    }
+}
