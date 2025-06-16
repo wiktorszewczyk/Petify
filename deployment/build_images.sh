6b9ffb73dfdcd6c88d74586217241d688a38c466
unknown <wiktorszewczyk21@gmail.com>
2025-06-16 14:07:54 +0200

    Deployment configurations

--- deployment/build_images.sh
+++ deployment/build_images.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-services=(config-server discovery-server gateway auth-server feed image shelter) # chat reservations funding
+services=(config-server discovery gateway auth-server feed image shelter) # chat reservations funding
 
 for service in "${services[@]}"
 do
     echo "Building image for service: $service..."
-    mvn spring-boot:build-image -f app/backend/$service/pom.xml \
+    mvn clean spring-boot:build-image -DskipTests \
+        -f app/backend/$service/pom.xml \
+        -Dspring-boot.build-image.builder=paketobuildpacks/builder-jammy-full \
         -Dspring-boot.build-image.imageName=$ACR_LOGIN_SERVER/petify-$service:latest
     docker push $ACR_LOGIN_SERVER/petify-$service:latest
 done
