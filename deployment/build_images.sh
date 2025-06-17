c04d784725f56aafd5f9d4c89a998454bb3da005
wiktorszewczyk <wiktorszewczyk21@gmail.com>
2025-06-17 03:54:32 +0200

    Added &&

--- deployment/build_images.sh
+++ deployment/build_images.sh
@@ -3,10 +3,10 @@ services=(config-server discovery auth-server chat feed funding image reservatio
 
 for service in "${services[@]}"
 do
-    echo "Building image for service: $service..."
+    echo "Building image for service: $service..." && \
     mvn clean spring-boot:build-image -DskipTests \
         -f app/backend/$service/pom.xml \
         -Dspring-boot.build-image.builder=paketobuildpacks/builder-jammy-full \
-        -Dspring-boot.build-image.imageName=$ACR_LOGIN_SERVER/petify-$service:latest
+        -Dspring-boot.build-image.imageName=$ACR_LOGIN_SERVER/petify-$service:latest && \
     docker push $ACR_LOGIN_SERVER/petify-$service:latest
 done
