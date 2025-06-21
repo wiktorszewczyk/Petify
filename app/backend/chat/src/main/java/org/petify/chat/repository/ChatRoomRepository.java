5a0733c3ce2a5729fb95d1038fd182a2a27c03e8
jhynasinski <247674@edu.p.lodz.pl>
2025-06-21 18:11:37 +0200

    fix

--- app/backend/chat/src/main/java/org/petify/chat/repository/ChatRoomRepository.java
+++ app/backend/chat/src/main/java/org/petify/chat/repository/ChatRoomRepository.java
@@ -1,8 +1,9 @@
 package org.petify.chat.repository;
 
 import org.petify.chat.model.ChatRoom;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query; // Upewnij się, że ten import jest dodany
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +14,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     Optional<ChatRoom> findByPetIdAndUserName(Long petId, String userName);
 
-    @Query("SELECT r FROM ChatRoom r " +
-            "WHERE r.userName = :login OR r.shelterName = :login " +
-            "ORDER BY r.lastMessageTimestamp DESC NULLS LAST")
+    @Query("SELECT r FROM ChatRoom r "
+            + "WHERE r.userName = :login OR r.shelterName = :login "
+            + "ORDER BY r.lastMessageTimestamp DESC NULLS LAST")
     List<ChatRoom> findAllRoomsForUserSorted(String login);
-
 }
