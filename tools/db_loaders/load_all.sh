3e5dd0158eae42a879e80c13f87f9d650a770244
wiktorszewczyk <wiktorszewczyk21@gmail.com>
2025-06-21 19:10:17 +0200

    Removed token

--- tools/db_loaders/load_all.sh
+++ tools/db_loaders/load_all.sh
@@ -1,5 +1,5 @@
 # Fill out and uncomment
-export JWT_ADMIN="<your_jwt_admin_token>"
+# export JWT_ADMIN="<your_jwt_admin_token>"
 export BASE_URL="http://localhost:8222"
 
 python3 tools/db_loaders/shelter_loader.py
